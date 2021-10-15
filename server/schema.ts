654640714ed1b5b2e5f60f7d5904c2f14e1cca59
Pascal Martineau <pascal@lewebsimple.ca>
2021-10-15 09:04:41 -0400

    Generate schema.graphql

--- server/schema.ts
+++ server/schema.ts
@@ -1,6 +1,10 @@
+import { resolve } from "path";
 import { makeSchema } from "nexus";
 import * as types from "./types";
 
 export const schema = makeSchema({
   types,
+  outputs: {
+    schema: resolve(process.cwd(), "server/generated/schema.graphql"),
+  },
 });
