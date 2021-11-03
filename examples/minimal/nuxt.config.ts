32948bd9a63a5c48f9ac8635c084377c30397423
Pascal Martineau <pascal@lewebsimple.ca>
2021-11-03 14:08:54 -0400

    Use VERCEL_URL

--- examples/minimal/nuxt.config.ts
+++ examples/minimal/nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from "nuxt3";
 export default defineNuxtConfig({
   buildModules: ["@nuxt3-graphql/codegen", "@nuxt3-graphql/urql"],
   urql: {
-    url: process.env.GRAPHQL_URL || "http://localhost:3000/api/graphql",
+    url: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/graphql` : "http://localhost:3000/api/graphql",
   },
 });
