c66d736dd4812e5efb51a3dc33246c6955f93dce
Pascal Martineau <pascal@lewebsimple.ca>
2021-12-15 22:05:43 -0500

    Fix @graphql-codegen/plugin-helpers external

--- packages/codegen/build.config.ts
+++ packages/codegen/build.config.ts
@@ -3,5 +3,12 @@ import { BuildConfig } from "unbuild";
 export default {
   declaration: true,
   entries: ["./src/index"],
-  externals: ["@graphql-codegen/cli", "@nuxt/kit", "@nuxt/schema", "consola", "graphql"],
+  externals: [
+    "@graphql-codegen/cli",
+    "@graphql-codegen/plugin-helpers",
+    "@nuxt/kit",
+    "@nuxt/schema",
+    "consola",
+    "graphql",
+  ],
 } as BuildConfig;
